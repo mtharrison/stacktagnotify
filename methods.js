8a29d2a5f442d07d473294b3be04a761f7a3848c
Matt Harrison <hi@matt-harrison.com>
2015-02-23 21:26:15 +0000

    Tabs => spaces

--- methods.js
+++ methods.js
@@ -3,18 +3,18 @@ var Soclient = require('./soclient');
 
 module.exports = function (server) {
 
-	server.method('getTags', function (tag, callback) {
+    server.method('getTags', function (tag, callback) {
 
-		Soclient.get('questions', {
-		    order: 'desc',
-		    page: 1,
-		    pagesize: 10,
-		    sort: 'creation',
-		    tagged: tag,
-		    site: 'stackoverflow'
-		}, function (err, data) {
-		    
-		    callback(err, data);
-		});
-	}, {});
+        Soclient.get('questions', {
+            order: 'desc',
+            page: 1,
+            pagesize: 10,
+            sort: 'creation',
+            tagged: tag,
+            site: 'stackoverflow'
+        }, function (err, data) {
+            
+            callback(err, data);
+        });
+    }, {});
 };
